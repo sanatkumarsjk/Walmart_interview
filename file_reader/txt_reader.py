daf7ce53f14f0719da67dc520e2de798e3a13df1
sanatkumarsjk <sanatkumarsjk@gmail.com>
2019-09-25 14:24:18 -0400

    txt reader done.

---
+++ file_reader/txt_reader.py
@@ -0,0 +1,20 @@
+from file_reader.request_reader import RequestReader
+import pandas as pd
+
+class TxtReader(RequestReader):
+
+    def read_file(self, filename):
+        try:
+            data = pd.read_csv(filename, sep=' ', header=None)
+            request = [[], []]
+            for index, row in data.iterrows():
+                request[0].append(row[0])
+                request[1].append(row[1]+0)
+            return request
+        except FileNotFoundError:
+            print('Invalid file name; please provide valid file path as first argument. E.g. python main.py test.txt')
+        except:
+            print('Invalid data in file, please provide a valid file')
+        return False
+
+
